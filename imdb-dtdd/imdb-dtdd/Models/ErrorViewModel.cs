358d045a7572da217dacf09d5a355a67a6df444c
sakshimoff <stonewing97@gmail.com>
2019-04-13 23:51:40 +0100

    mvc layout initialised

---
+++ imdb-dtdd/imdb-dtdd/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace imdb_dtdd.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
